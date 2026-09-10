925bd350382cfa053799c5decdcc87acb607b2da
rd-sloan <sloan@rushdownstudio.com>
2026-09-10 15:04:12 -0400

    stub out a basic board class
    We're doing connect 4 baby, luau will handle the ai

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,101 @@ static std::string readFile(const std::string& path)
 	return ss.str();
 }
 
+
+class Board
+{
+public:
+	// todo instead of class functions lets just make these functions that take in a board.
+	void Init()
+	{
+
+		for (int row = 0; row < NumberOfRows; ++row)
+		{
+			for (int col = 0; col < NumberOfColumns; ++col)
+			{
+				m_board[row][col] = EmptyMarker;
+			}
+		}
+
+		for (int i = 0; i < NumberOfColumns; ++i)
+		{
+			m_fullColumns[i] = false;
+		}
+	}
+
+	std::string GetDisplayString()
+	{
+		std::string displayString;
+		for (int row = 0; row < NumberOfRows; ++row)
+		{
+			for (int col = 0; col < NumberOfColumns; ++col)
+			{
+				displayString += std::to_string(m_board[row][col]) + " ";
+			}
+			displayString += "\n";
+		}
+
+		return displayString;
+	}
+
+	bool IsColumnFull(uint8_t colNumber)
+	{
+		if (colNumber < NumberOfColumns)
+		{
+			return m_fullColumns[colNumber];
+		}
+
+		return true; // if column doesn't exist its technically fully
+	}
+
+	bool TryPlayMove(uint8_t colNumber, uint8_t playerNum)
+	{
+		if (colNumber >= NumberOfColumns)
+		{
+			std::fprintf(stderr, "invalid colNum %i", colNumber);
+			return false;
+		}
+
+		if (playerNum != 1 && playerNum != 2)
+		{
+			std::fprintf(stderr, "invalid playerNum %i", playerNum);
+			return false;
+		}
+
+		if (IsColumnFull(colNumber))
+		{
+			std::fprintf(stderr, "column is full %i", colNumber);
+			return false;
+		}
+
+		// try to find where 'gravity' would take the piece
+		bool validMove = false;
+		for (int i = NumberOfRows; i >= 0; --i)
+		{
+			if (m_board[i][colNumber] == EmptyMarker)
+			{
+				m_board[i][colNumber] = playerNum;
+				validMove = true;
+				break;
+			}
+		}
+
+		return validMove;
+	}
+
+private:
+	static const uint8_t NumberOfColumns = 7;
+	static const uint8_t NumberOfRows = 6;
+	static const uint8_t EmptyMarker = 0;
+	uint8_t m_board[NumberOfRows][NumberOfColumns];
+
+	//helper to make querying for full columns ez
+	bool m_fullColumns[NumberOfColumns];
+
+};
+
+
+
 int main()
 {
 	std::string source = readFile(std::string(SCRIPTS_DIR) + "/script.luau");
@@ -47,5 +142,27 @@ int main()
 	}
 
 	lua_close(luaState);
+
+	Board board;
+	board.Init();
+	std::fprintf(stdout, board.GetDisplayString().c_str());
+	std::fprintf(stdout, "\nPlay column 3, player 1\n");
+	board.TryPlayMove(3, 1);
+	std::fprintf(stdout, board.GetDisplayString().c_str());
+	std::fprintf(stdout, "\nPlay column 3, player 2\n");
+	board.TryPlayMove(3, 2);
+	std::fprintf(stdout, board.GetDisplayString().c_str());
+	std::fprintf(stdout, "\nPlay column 4, player 1\n");
+	board.TryPlayMove(4, 1);
+	std::fprintf(stdout, board.GetDisplayString().c_str());
+	std::fprintf(stdout, "\nPlay column 2, player 2\n");
+	board.TryPlayMove(2, 2);
+	std::fprintf(stdout, board.GetDisplayString().c_str());
+	std::fprintf(stdout, "\nPlay column 4, player 1\n");
+	board.TryPlayMove(4, 1);
+	std::fprintf(stdout, board.GetDisplayString().c_str());
+
+
 	return 0;
 }
+
