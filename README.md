465b640d0e12d221080fc38a6a93dd745cf14f55
rd-sloan <sloan@rushdownstudio.com>
2026-09-10 11:17:26 -0400

    Initial implementation
    Just read a luau file and execute it.

---
+++ README.md
@@ -0,0 +1,3 @@
+# luau-cpp
+
+Just a place to play around with luau and c++
---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include "lua.h"
+#include "lualib.h"
+#include "luacode.h"
+
+static std::string readFile(const char* path)
+{
+	std::ifstream file(path);
+	std::stringstream ss;
+	ss << file.rdbuf();
+	return ss.str();
+}
+
+int main()
+{
+	std::string source = readFile("scripts/script.luau");
+
+	// compile source code into bytecode
+	size_t bytecodeSize = 0;
+	char* bytecode = luau_compile(source.c_str(), source.size(), nullptr, &bytecodeSize);
+
+	// Create a Luau VM state
+	lua_State* luaState = luaL_newstate();
+	luaL_openlibs(luaState); // opens standard library (print, math, string, etc)
+
+	// Load compiled bytecode into VM
+	int result = luau_load(luaState, "script", bytecode, bytecodeSize, 0);
+	free(bytecode);
+
+	if (result != 0)
+	{
+		std::fprintf(stderr, "failed to load script:%s\n", lua_tostring(luaState, -1));
+		lua_close(luaState);
+		return 1;
+	}
+
+	// run the luaa script
+	if (lua_pcall(luaState, 0, LUA_MULTRET, 0) != LUA_OK)
+	{
+		std::fprintf(stderr, "Runtime error: %s\n", lua_tostring(luaState, -1));
+		lua_close(luaState);
+		return 1;
+	}
+
+	lua_close(luaState);
+	return 0;
+}
